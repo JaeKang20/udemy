0ad8ecbb4ba6062565109ab14e8406470d156a83
jaekang20 <jkchoi7091@knou.ac.kr>
2022-07-22 19:32:41 +0900

    추가해보기

--- udemy/udemyForGithub.java
+++ udemy/udemyForGithub.java
@@ -4,6 +4,7 @@ public class udemyForGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//유데미코드 연습을 앞으로 깃허브에 커밋하기 위해서 연습해보는 폴더
+		//앞으로 내가 배우는 모든 코드들을 커밋해볼테다
 	}
 
 }
